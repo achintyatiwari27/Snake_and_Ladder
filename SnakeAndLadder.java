84abb11f72de03f74023803396a7de01864e5529
Achintya <achintyatiwari27>
2021-08-28 07:51:31 +0530

    [Added]:Code to get random number

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,6 +3,9 @@ package com.BridgeLabz;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder program");
-        int startPosition = 0;         
+        int startPosition = 0; 
+ 
+        int diceNumber = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+        System.out.println(diceNumber);
     }
 }
