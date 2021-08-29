5594d8a547a8b7cd03398802f6d053a91bd9b87c
Achintya <achintyatiwari27>
2021-08-29 19:28:27 +0530

    [Refactored]:Code to check options

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,11 +1,27 @@
 package com.BridgeLabz;
 
 public class SnakeAndLadder {
+
+    public static final int NO_PLAY = 0;
+    public static final int IS_SNAKE = 1;
+    public static final int IS_LADDER = 2;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder program");
-        int startPosition = 0; 
- 
+
         int diceNumber = (int) (Math.floor(Math.random() * 10) % 6 + 1);
-        System.out.println(diceNumber);
+        int option = (int) (Math.floor(Math.random() *10) % 3 + 1);
+
+        int startPosition = 0;
+        if (option == IS_SNAKE) {
+            System.out.println("Snake for the player");
+            startPosition = startPosition - diceNumber;
+        } else if (option == IS_LADDER) {
+            System.out.println("Ladder for the player");
+            startPosition = startPosition + diceNumber;
+        } else {
+            System.out.println("No play for the player");
+        }
+        System.out.println("Position" + startPosition);
     }
 }
