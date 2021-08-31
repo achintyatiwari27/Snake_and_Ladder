d706c5fcf690582bac903f805b27cb4cf298eb6a
Achintya <achintyatiwari27>
2021-08-31 18:14:37 +0530

    [Refactored]:Repeat till winning position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -10,37 +10,30 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder program");
 
-        int diceCount = 0;
         int diceNumber = 0;
-        int position = 0;
+        int option = 0;
         int playerPosition = 0;
 
-        for (playerPosition=0; playerPosition <=100; playerPosition++) {
+        for (playerPosition = 0; playerPosition <= 100; playerPosition++) {
             diceNumber = (int) (Math.floor(Math.random() * 10) % 6 + 1);
-            System.out.println("Position" + playerPosition);
-            diceCount = playerPosition + diceNumber;
-            System.out.println("Dicecount" + diceCount);
-        }
-            int option = (int) (Math.floor(Math.random() * 10) % 3 + 1);
-
-            int startPosition = 0;
-            if (option == IS_SNAKE) {
-                System.out.println("Snake for the player");
-                startPosition = startPosition - diceNumber;
-                System.out.println(startPosition);
-                if (position < 0) {
-                    position = 0;
+            System.out.println(diceNumber);
+            option = (int) (Math.floor(Math.random() * 10) % 3);
+            if (option == 1) {
+                System.out.println("Snake for thr player");
+                playerPosition -= diceNumber;
+                if (playerPosition < 0) {
+                    playerPosition = 0;
                 }
-            } else if (option == IS_LADDER) {
+            } else if (option == 2) {
                 System.out.println("Ladder for the player");
-                startPosition = startPosition + diceNumber;
-                System.out.println(startPosition);
-                if (position > 100) {
-                    position -= diceNumber;
+                playerPosition += diceNumber;
+                if (playerPosition > 100) {
+                    playerPosition -= diceNumber;
                 }
             } else {
-                System.out.println("No play for the player");
+                System.out.println("No Play for the player");
             }
-            System.out.println(diceNumber);
+            System.out.println("Position" + playerPosition);
+        }
     }
 }
